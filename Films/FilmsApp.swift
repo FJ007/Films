d46233bba2b1419d790c4387cd34071a97959050
Javier Fernández <kewo_007@hotmail.com>
2020-10-30 13:25:54 +0100

    Initial Commit

---
+++ Films/FilmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmsApp.swift
+//  Films
+//
+//  Created by Javier Fernández on 30/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct FilmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
